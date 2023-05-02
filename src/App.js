81c721540007c8a6639289efafcfa023359bcfcf
albinaabykanova <abykanovaalbina@gmail.com>
2023-05-02 23:48:02 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
+
 
 
 function App() {
-  return (
-    <div className="App">
-        <Header />
-        <Main /> 
-    </div>
-  );
-}
+  
+
+   return (
+    <>
+       
+    </>
+    
+   )
+  }
+
 
 export default App;
